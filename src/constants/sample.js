01f35e9587e9a07c3a1ba430833986d442fca955
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-07-20 20:29:55 +0530

    constants-added

---
+++ src/constants/sample.js
@@ -0,0 +1,27 @@
+export const samplePosts = [
+    {
+      id: 1,
+      content: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      category: 0,
+      createdAt: '2024-07-20T14:55:35.042Z',
+      author: {
+        id: 1,
+        name: 'John Doe',
+        avatar: 'https://randomuser.me/api/portraits/med/men/35.jpg'
+      },
+      commentsCount: 24
+    },
+    {
+      id: 2,
+      content: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      category: 1,
+      createdAt: '2024-07-20T14:55:35.042Z',
+      author: {
+        id: 2,
+        name: 'Jane Doe',
+        avatar: 'https://randomuser.me/api/portraits/med/women/57.jpg'
+      },
+      commentsCount: 12
+    }
+  ];
+  
