aae8a566ec2c623fadd71b259505213e25594a05
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-07-21 20:56:01 +0530

    logout-option-added

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -6,59 +6,72 @@ import CreatePost from './CreatePost'
 
 import Modal from '../Modal'
 import { useLogin } from '../../context/LoginContext'
+import { logout } from '../../services/auth'
+import { ROUTES } from '../../constants/routes'
+import { useNavigate } from 'react-router-dom'
 
 const Blog = () => {
-    const {currentUser,setShowModal,showModal} = useLogin();
+    const { currentUser,setCurrentUser, setShowModal, showModal } = useLogin();
     const [posts, setPosts] = useState(samplePosts)
+    const navigate = useNavigate()
 
 
     // console.log("post", posts)
     // console.log(currentUser)
 
     const handlePost = (postContent, category) => {
-        if(currentUser?.id){
+        if (currentUser?.id) {
             const newPost = {
                 id: Math.floor(100 * Math.random()),
                 content: postContent,
                 category,
                 createdAt: new Date().toISOString(),
                 author: {
-                  id: currentUser.id,
-                  name: currentUser.username,
-                  avatar: currentUser.avatar ?? 'https://randomuser.me/api/portraits/women/43.jpg'
+                    id: currentUser.id,
+                    name: currentUser.username,
+                    avatar: currentUser.avatar ?? 'https://randomuser.me/api/portraits/women/43.jpg'
                 },
                 commentsCount: 0
-              };
-            
-              setPosts([newPost, ...posts]);
+            };
+
+            setPosts([newPost, ...posts]);
         }
-        else{
+        else {
             // alert("please login first")
             setShowModal(true)
         }
-      };
-      
+    };
+
+
+    const handleLogout = ()=>{
+          logout()
+          setCurrentUser(null)
+    }
 
     return (
         <>
-        <div className="blog">
-            <div className='blog__content'>
-                <h1 className='blog__content__title'>Hello </h1>
-                <div className='blog__content__description'>
-                    How are you doing today? Would you like to share something with the community 🤗
-                </div>
+            <div className="blog">
+                <div className='blog__content'>
+                    <h1 className='blog__content__title'>Hello {currentUser?.username ?? 'Guest'}</h1>
+                    <div className='blog__content__description'>
+                        How are you doing today? Would you like to share something with the community 🤗
+                    </div>
 
-                <div className='blog__content__posts'>
-                    <CreatePost handlePost={handlePost} />
-                    {
-                        posts.map((post) => (
-                            <Post key={post.id} {...post} />
-                        ))
-                    }
+                    <div className='blog__content__posts'>
+                        <CreatePost handlePost={handlePost} />
+                        {
+                            posts.map((post) => (
+                                <Post key={post.id} {...post} />
+                            ))
+                        }
+                    </div>
                 </div>
+                {
+                    currentUser?.id &&
+                    <div className='blog__logout' onClick={handleLogout}>Logout</div>
+                }
             </div>
-        </div>
-        {showModal && <Modal />}
+            {showModal && <Modal />}
         </>
     )
 }
